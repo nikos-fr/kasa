536f256f275bdea8b8250f4635d78d16b58ba554
nikosfr <nicolousa.dev@gmail.com>
2023-03-21 16:45:26 +0100

    page 404 OK

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,13 @@ function NotFound() {
     <div>
       <h1>404</h1>
       <h3 className='txterror'>Oups! La page que vous demandez n'existe pas.</h3>
-  
+      <ul className='backacceuil'>
+        <li className='active-link'>
+        <NavLink to="/" className={({isActive}) => (isActive ? "active-link" : undefined)}>
+            Retourner sur la page d'acceuil
+          </NavLink>
+        </li>          
+      </ul>
     </div>
   )
 }
