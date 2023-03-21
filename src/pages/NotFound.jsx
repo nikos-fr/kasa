b0636f6978bddc9d65b996adbcbdbf846e9f597a
nikosfr <nicolousa.dev@gmail.com>
2023-03-21 12:40:02 +0100

    404 EN COUR

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+import "./notfound.css"
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <h1>404</h1>
+      <h3 className='txterror'>Oups! La page que vous demandez n'existe pas.</h3>
+  
+    </div>
   )
 }
 
